00ed90c58ff31266e9dd82480c2f78e5388236f9
voody77 <vadim@kruchinin.moscow>
2019-12-22 15:07:25 +0300

    Add bootstrap styles to devise forms

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module Bbq
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
+    #config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
