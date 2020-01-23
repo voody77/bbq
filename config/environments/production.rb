a7ca3682696d9ac9c8db139df37914b30c58724e
voody77 <vadim@kruchinin.moscow>
2020-01-23 23:03:30 +0300

    refactor production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,13 +97,13 @@ Rails.application.configure do
 
   # А это для SendGrid
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
+    address:              'smtp.sendgrid.net',
+    port:                 '587',
+    authentication:       :plain,
+    user_name:            ENV['SENDGRID_USERNAME'],
+    password:             ENV['SENDGRID_PASSWORD'],
+    domain:               'heroku.com',
+    enable_starttls_auto: true
   }
 
   # Inserts middleware to perform automatic connection switching.
