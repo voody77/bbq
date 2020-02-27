b0dbcf46c85ecceca4b138fb08be67a3f629d915
voody77 <vadim@kruchinin.moscow>
2020-02-27 16:05:20 +0300

    Remove validate in subscription.rb

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,7 +3,6 @@ class Subscription < ApplicationRecord
   belongs_to :user, optional: true
 
   validates :event, presence: true
-  validate :can_subscribe?, on: :create, if: -> { user.present?}
   validates :user_name, presence: true, unless: -> { user.present? }
   validate :email_used?, on: :create, unless: -> { user.present? }
   validates :user_email, presence: true, 'valid_email_2/email': true, unless: -> { user.present? }
@@ -24,9 +23,5 @@ class Subscription < ApplicationRecord
 
   def email_used?
     errors.add(:user_email, I18n.t('devise.messages.subscription.email_used')) if User.where(email: user_email).present?
-  end
-
-  def can_subscribe?
-    errors.add(:user, I18n.t('devise.messages.subscription.cant_subs')) if event.user == user
   end
 end
